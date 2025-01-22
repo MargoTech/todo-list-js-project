ba74466dce9a8983094bd7c1c320fffb2637a21e
MargoTech <margobartashevich@gmail.com>
2025-01-22 16:59:38 +0100

    adding JS

--- script.js
+++ script.js
@@ -3,11 +3,22 @@ const addTaskBtn = document.getElementById("addTaskBtn");
 const taskList = document.getElementById("taskList");
 
 addTaskBtn.addEventListener("click", function () {
-  const taskText = taskInput.ariaValueText.trim();
+  const taskText = taskInput.value.trim();
   if (taskText) {
     const li = document.createElement("li");
     li.textContent = taskText;
+
+    const deleteBtn = document.createElement("button");
+    deleteBtn.textContent = "Delete";
+    deleteBtn.style.marginLeft = "10px";
+
+    deleteBtn.addEventListener("click", () => {
+      li.remove();
+    });
+
+    li.appendChild(deleteBtn);
+
     taskList.appendChild(li);
-    taskInput.nodeValue = "";
+    taskInput.value = "";
   }
 });
